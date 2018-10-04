7aa5bc5d6f4c84326ad76d02fe95ab2d10b0717f
Shmully Wolfson <shmullywolfson@gmail.com>
2018-10-04 13:36:28 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(:id)
+  end
+
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(store: params[:store], coupon_code: params[:coupon_code] )
+
+    redirect_to coupon_path(@coupon)
+  end
+
+end
--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -6,15 +6,18 @@ describe CouponsController do
     context "with valid attributes" do
       it "creates a new coupon" do
         expect{
-          post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
+          post :create, params: { :coupon_code => "ASD123", :store => "Dean and Deluca" }
         }.to change(Coupon,:count).by(1)
       end
-      
+
       it "redirects to the new coupon" do
-        post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
+        post :create, params: { :coupon_code => "ASD123", :store => "Dean and Deluca" } 
         expect(response).to redirect_to Coupon.last
       end
     end
   end
 
 end
+
+# get :show, params: { id: 1 }, flash: { notice: "This is a flash message" },
+#   session: nil # Can safely be omitted.
